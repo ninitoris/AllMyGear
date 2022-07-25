3aa0f5c1b212d0a45308065c7feeacb9edf89b8d
SSirotinin <stassir99@gmail.com>
2022-07-25 11:03:32 +0300

    header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './core/home/home.component';
 import { HeaderComponent } from './core/home/header/header.component';
+import { ChecklistComponent } from './core/home/checklist/checklist.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    HeaderComponent
+    HeaderComponent,
+    ChecklistComponent
   ],
   imports: [
     BrowserModule,
